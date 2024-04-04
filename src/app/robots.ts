530a3871940eb7c841b7a5d3a0c603bad18cdceb
amaan-mohib <amaan.mohib@hotmail.com>
2024-04-04 14:18:05 +0530

    added robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from "next";
+import { headers } from "next/headers";
+
+export default function robots(): MetadataRoute.Robots {
+  const headerList = headers();
+  const host = headerList.get("host");
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: ["/"],
+      disallow: "/private/",
+    },
+    sitemap: `https://${host}/sitemap.xml`,
+  };
+}
