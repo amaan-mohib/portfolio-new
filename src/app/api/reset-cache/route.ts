2ddb11e986a4fc65ba0a9e53bcb569db06effc96
amaan-mohib <amaan.mohib@hotmail.com>
2025-11-22 21:05:11 +0530

    refactor reset cache

--- src/app/api/reset-cache/route.ts
+++ src/app/api/reset-cache/route.ts
@@ -3,7 +3,28 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {
   try {
-    await fetch(process.env.MAIN_LAMBDA_FUNCTION_URL!);
+    const url = 'https://api.imagekit.io/v1/files/purge';
+    const JSON_FILENAMES = ["info.json", "home.json", "links.json", "projects.json"];
+
+    await Promise.all(
+      JSON_FILENAMES.map((key) => {
+          const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+              Authorization: `Basic ${process.env.IMAGEKIT_AUTH_TOKEN}`
+            },
+            body: JSON.stringify({
+              url: `${process.env.JSON_DATA_BASE_URL}/${key}`
+            })
+          };
+
+          return fetch(url, options);
+        }
+      )
+    );
+
 
     return NextResponse.json({
       status: 200,
